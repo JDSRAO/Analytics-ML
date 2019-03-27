79d66cf1b0a07aadd4ebbf47385ad95558ad79ca
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-03-27 12:32:38 +0530

    Project setup

---
+++ ML.NET/Samples/HelloWorld/HelloWorld/DataStructrues/IrisPrediction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.DataStructrues
+{
+    public class IrisPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+}
--- ML.NET/Samples/HelloWorld/HelloWorld/Program.cs
+++ ML.NET/Samples/HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // STEP 2: Create a ML.NET environment
+            var mlContext = new MLContext();
         }
     }
 }
