3cbb44b1fa1474e568231e3c00b12b82fedcf521
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-03-27 17:07:52 +0530

    Solution setup

---
+++ ML.NET/Samples/IrisFlowers.Clustering/IrisFlowers.Clustering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IrisFlowers.Clustering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
