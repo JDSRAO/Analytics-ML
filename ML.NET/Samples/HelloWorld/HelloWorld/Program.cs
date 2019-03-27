e8131422278011cf1306b138860aa81a3f191ff1
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-03-27 13:33:12 +0530

    updated program

--- ML.NET/Samples/HelloWorld/HelloWorld/Program.cs
+++ ML.NET/Samples/HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.ML;
+﻿using HelloWorld.DataStructrues;
+using Microsoft.Data.DataView;
+using Microsoft.ML;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,38 @@ namespace HelloWorld
         {
             // STEP 2: Create a ML.NET environment
             var mlContext = new MLContext();
+
+            var trainingDataView = mlContext.Data.LoadFromTextFile<IrisData>(path: "iris-data.txt", hasHeader: false, separatorChar: ',');
+
+            // STEP 3: Transform your data and add a learner
+            // Assign numeric values to text in the "Label" column, because only
+            // numbers can be processed during model training.
+            // Add a learning algorithm to the pipeline. e.g.(What type of iris is this?)
+            // Convert the Label back into original text (after converting to number in step 3)
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                .AppendCacheCheckpoint(mlContext)
+                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: "Label", featureColumnName: "Features"))
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            // STEP 4: Train your model based on the data set
+            var model = pipeline.Fit(trainingDataView);
+
+            // STEP 5: Use your model to make a prediction
+            // You can change these numbers to test different predictions
+            var prediction = model.CreatePredictionEngine<IrisData, IrisPrediction>(mlContext).Predict(
+                new IrisData()
+                {
+                    SepalLength = 3.3f,
+                    SepalWidth = 1.6f,
+                    PetalLength = 0.2f,
+                    PetalWidth = 5.1f,
+                });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+
+            Console.WriteLine("Press any key to exit....");
+            Console.ReadLine();
         }
     }
 }
