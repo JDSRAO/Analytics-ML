9d35d3f8ffa0a40778c6cc2c0b9d984258e7d64e
JDSRAO <srinivas@cloudthing.com>
2019-03-27 18:00:43 +0530

    Project setup

---
+++ ML.NET/Samples/Alcohol.Consumption/Alcohol.Consumption/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alcohol.Consumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
