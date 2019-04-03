ad6b75362925a6a00c16d8cb39891080303909db
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-04-03 17:22:44 +0530

    Save commit

---
+++ ML.NET/Samples/Alcohol.Consumption/Alcohol.Consumption/Models/ReflectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Alcohol.Consumption.Models
+{
+    public class ReflectionExtension
+    {
+        public static List<string> GetParameterNames(Type type)
+        {
+            var flags = BindingFlags.Public | BindingFlags.Instance;
+            var properties = type.GetProperties(flags);
+            List<string> names = new List<string>();
+            foreach (var property in properties)
+            {
+                if(property.CanRead)
+                {
+                    var name = property.Name;
+                    names.Add(property.Name);
+                }
+            }
+
+            return names;
+        }
+    }
+}
