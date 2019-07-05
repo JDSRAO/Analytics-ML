f11c4e2934590035f9ac2e8dc06b77fd0e8c54c2
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-07-05 14:06:58 +0530

    Path issue fix

--- ML.NET/Samples/Alcohol.Consumption/Alcohol.Consumption/Program.cs
+++ ML.NET/Samples/Alcohol.Consumption/Alcohol.Consumption/Program.cs
@@ -8,35 +8,42 @@ namespace Alcohol.Consumption
 {
     class Program
     {
-        const string fileName = "drinks.csv";
+        const string fileName = @"data\drinks.csv";
 
         static void Main(string[] args)
         {
-            var path = GetFilePath(fileName);
-            var mlContext = new MLContext(seed: 0);
-            Console.WriteLine($"Reading data from path: {path}");
-            var dataView = mlContext.Data.LoadFromTextFile<AlcoholData>(path, hasHeader: true, separatorChar: ',');
-            Console.WriteLine("Completed reading data from file \n");
+            try
+            {
+                var path = GetFilePath(fileName);
+                var mlContext = new MLContext(seed: 0);
+                Console.WriteLine($"Reading data from path: {path}");
+                var dataView = mlContext.Data.LoadFromTextFile<AlcoholData>(path, hasHeader: true, separatorChar: ',');
+                Console.WriteLine("Completed reading data from file \n");
 
-            Console.WriteLine("Trasformation of data \n");
-            var names = ReflectionExtension.GetParameterNames(typeof(AlcoholData));
-            var feature = names[0];
-            var dataColumns = names.Where(x => x != feature).ToArray();
-            var pipeline = mlContext.Transforms
-                .Conversion.MapValueToKey(feature)
-                .Append(mlContext.Transforms.Concatenate("Features", dataColumns))
-                .AppendCacheCheckpoint(mlContext)
-                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: feature, featureColumnName: "Features"))
-                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                Console.WriteLine("Trasformation of data \n");
+                var names = ReflectionExtension.GetParameterNames(typeof(AlcoholData));
+                var feature = names[0];
+                var dataColumns = names.Where(x => x != feature).ToArray();
+                var pipeline = mlContext.Transforms
+                    .Conversion.MapValueToKey(feature)
+                    .Append(mlContext.Transforms.Concatenate("Features", dataColumns))
+                    .AppendCacheCheckpoint(mlContext)
+                    .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: feature, featureColumnName: "Features"))
+                    .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
-            Console.WriteLine("Create prediction model");
-            var model = pipeline.Fit(dataView);
+                Console.WriteLine("Create prediction model");
+                var model = pipeline.Fit(dataView);
 
-            Console.WriteLine("Prediction of model");
+                Console.WriteLine("Prediction of model");
 
-            
 
-            Console.ReadKey();
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         static string GetFilePath(string fileName)
